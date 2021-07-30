9cce9ec50472ed8e8c8b3adb2030362bddc0abd6
Jamal Dahbur <jd@Jamals-MacBook-Pro.local>
2021-07-30 09:14:06 -0500

    trying to add gatsby-plugin-resolve-src to fix

---
+++ src/components/header.js
@@ -0,0 +1,75 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import "assets/scss/index-new.scss"
+
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-hand-peace-o"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>
+        <a
+                href="https://github.com/dahburj"
+                target="_blank"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+        </h1>
+      </div>
+    </div>
+    <nav>
+      <ul>
+      {/*
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('main')
+            }}
+          >
+            main
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        */}
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('playground')
+            }}
+          >
+            Playground
+          </button>
+        </li>
+
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
